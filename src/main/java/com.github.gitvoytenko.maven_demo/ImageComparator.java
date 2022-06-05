2d6a1d95fcb2c0f47bcb5a99ba61e5491d5f0787
Dmitriy Voytenko <mailvoytenko@gmail.com>
2022-06-05 20:52:03 +0300

    Code refactoring in ImageComparatorTest class.

--- src/main/java/com.github.gitvoytenko.maven_demo/ImageComparator.java
+++ src/main/java/com.github.gitvoytenko.maven_demo/ImageComparator.java
@@ -6,18 +6,20 @@ import com.github.romankh3.image.comparison.model.ImageComparisonState;
 
 import java.awt.image.BufferedImage;
 
-/* Class created for answering, does two images the same or not.
-* */
+/**
+ * Class created for answering, does two images the same or not.
+ */
 
 
 public class ImageComparator {
 
-    /* Method, which says the same images ot not.
+    /**
+    * Method, which says the same images ot not.
     *
     * @param image1 image1 for comparison
     * @param image2 image2 for comparison
     * @return return true, if images are the same, false - otherwise.
-    * */
+    */
 
     public boolean isTheSameImages(BufferedImage image1, BufferedImage image2){
         // Create ImageComparison object
